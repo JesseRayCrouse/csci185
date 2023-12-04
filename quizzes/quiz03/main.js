23c4c7d8837769c0e891be014f55c301cc3bde02
Jesse <jcrouse10903@gmail.com>
2023-12-04 16:22:36 -0500

    quiz 3

---
+++ quizzes/quiz03/main.js
@@ -0,0 +1,33 @@
+function makeRed() {
+    console.log('Change background to red');
+    document.querySelector('#square1').style.backgroundColor = 'red';
+}
+
+function makeOrange() {
+    console.log('Change background to orange');
+    document.querySelector('#square2').style.backgroundColor = 'orange';
+}
+
+function makeYellow() {
+    console.log('Change background to yellow');
+    document.querySelector('#square3').style.backgroundColor = 'yellow';
+}
+
+function makeGreen() {
+    console.log('Change background to green');
+    document.querySelector('#square4').style.backgroundColor = 'green';
+}
+
+function makeBlue() {
+    console.log('Change background to blue');
+    document.querySelector('#square5').style.backgroundColor = 'blue';
+}
+
+function makePurple() {
+    console.log('Change background to purple');
+    document.querySelector('#square6').style.backgroundColor = 'purple';
+}
+function makePink() {
+    console.log('Change background to pink');
+    document.querySelector('#square7').style.backgroundColor = 'hotpink';
+}
